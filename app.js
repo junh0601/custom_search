e9c8323bc0ae1cd9f15d6b3d55d25054bb7b6abb
junh0601 <junh0601@naver.com>
2022-07-02 08:31:09 +0900

    네이버 검색 추가

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 const search = document.querySelector("#search");
 const google = document.querySelector("#google");
+const naver = document.querySelector("#naver");
 const erase = document.querySelector("#erase");
 const required = document.querySelector("#required");
 
-function handleGoogle(event) {
-    event.preventDefault();
-    const searchText = search.value;
-    if (searchText == ""){
+
+function openUrl(link) {
+    if (search.value == ""){
         search.placeholder="검색어를 입력하세요"
     }else{
-        location.href = `https://www.google.com/search?q=${searchText}`
-        console.log(`search ${searchText} with google`);
+        location.href = link;
     }
+}
+
+function handleNaver(event) {
+    event.preventDefault();
+    const searchLink = `https://search.naver.com/search.naver?ie=UTF-8&query=${search.value}`;
+    openUrl(searchLink);
+}
+
+function handleGoogle(event) {
+    event.preventDefault();
+    const searchLink = `https://www.google.com/search?q=${search.value}`;
+    openUrl(searchLink);
 }
 
 function handleErase() {
     search.value = "";
 }
 
-erase.addEventListener("click",handleErase);
+
+
+erase.addEventListener("click", handleErase);
 google.addEventListener("click", handleGoogle);
-search.on
+naver.addEventListener("click", handleNaver);
