163fadda65604a70ebe6a166eef513078e992a71
junh0601 <junh0601@naver.com>
2022-07-02 08:02:09 +0900

    0702-08:02

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("hello");
+const search = document.querySelector("#search");
+const google = document.querySelector("#google");
+const erase = document.querySelector("#erase");
+const required = document.querySelector("#required");
+
+function handleGoogle(event) {
+    event.preventDefault();
+    const searchText = search.value;
+    if (searchText == ""){
+        search.placeholder="검색어를 입력하세요"
+    }else{
+        location.href = `https://www.google.com/search?q=${searchText}`
+        console.log(`search ${searchText} with google`);
+    }
+}
+
+function handleErase() {
+    search.value = "";
+}
+
+erase.addEventListener("click",handleErase);
+google.addEventListener("click", handleGoogle);
+search.on
